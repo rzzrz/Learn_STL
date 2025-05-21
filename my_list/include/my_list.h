5c33e8e9df9f315e70715273bb2257d7f980312b
2197947404@qq.com <rzz-rz@rzzrz.>
2025-05-21 22:34:38 +0800

    完成代码并提供测试代码,(还没测试）

--- my_list/include/my_list.h
+++ my_list/include/my_list.h
@@ -16,17 +16,18 @@ public:
   // 链表的节点结构体和迭代器
   struct list_node {
     using value_type = T;
-    using list_type = list_node*;
-    
+    using list_type = list_node *;
 
     list_type next;
     list_type prev;
     value_type value;
 
     list_node() : next(this), prev(this) {}
-    list_node(list_type next, list_type prev) {}
-    list_node(list_type next, list_type prev, const value_type &value) {}
-    list_node(list_type next, list_type prev, value_type &&value) {
+    list_node(list_type next, list_type prev) : next(next), prev(prev) {}
+    list_node(list_type next, list_type prev, const value_type &value)
+        : next(next), prev(prev), value(value) {}
+    list_node(list_type next, list_type prev, value_type &&value)
+        : next(next), prev(prev) {
       this->value = std::move(value);
     }
   };
@@ -41,9 +42,10 @@ public:
     using const_reference = const value_type &;
     using self = iterator;
 
+    friend class list;
   public:
     // 普通构造函数
-    iterator(list_node* node) { now_node = node; }
+    iterator(list_node *node) { now_node = node; }
     // 拷贝构造函数
     iterator(self &other) { now_node = other.now_node; }
     // 移动构造
@@ -51,27 +53,48 @@ public:
 
     // 重载运算符
     self &operator++() {
-      now_node = *static_cast<list_node>(now_node->next);
+      now_node = now_node->next;
       return *this;
     }
     self operator++(int) {
       self tmp = *this;
-      now_node = *static_cast<list_node>(now_node->next);
+      now_node = now_node->next;
       return tmp;
     }
 
     self &operator--() {
-      now_node = *static_cast<list_node>(now_node->prev);
+      now_node = now_node->prev;
       return *this;
     }
     self operator--(int) {
       self tmp = *this;
-      now_node = *static_cast<list_node>(now_node->prev);
+      now_node = now_node->prev;
       return tmp;
     }
 
     value_type &operator*() { return now_node->value; }
     value_type *operator->() { return &now_node->value; }
+
+    bool operator==(const iterator &other) const {
+      return this->now_node == other.now_node;
+    }
+    bool operator!=(const iterator &other) const {
+      return this->now_node != other.now_node;
+    }
+
+    self operator+(int n) {
+      self tmp = *this;
+      for (int i = 0; i < n; i++)
+        tmp++;
+      return tmp;
+    }
+
+    self operator-(int n) {
+      self tmp = *this;
+      for (int i = 0; i < n; i++)
+        tmp--;
+      return tmp;
+    }
 
     self &operator=(const self &other) {
       now_node = other.now_node;
@@ -83,10 +106,85 @@ public:
     }
 
   private:
-    list_node* now_node;
+    list_node *now_node;
   };
 
-  class reverse_iterator {};
+  class reverse_iterator {
+  public:
+    // 双向迭代器
+    using iterator_tag = bidirectional_iterator_tag;
+    using value_type = T;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+    using self = reverse_iterator;
+
+    friend class list;
+
+  public:
+    // 普通构造函数
+    reverse_iterator(list_node *node) { now_node = node; }
+    // 拷贝构造函数
+    reverse_iterator(self &other) { now_node = other.now_node; }
+    // 移动构造
+    reverse_iterator(self &&other) { now_node = other.now_node; }
+
+    // 重载运算符
+    self &operator++() {
+      now_node = now_node->prev;
+      return *this;
+    }
+    self operator++(int) {
+      self tmp = *this;
+      now_node = now_node->prev;
+      return tmp;
+    }
+
+    self &operator--() {
+      now_node = now_node->next;
+      return *this;
+    }
+    self operator--(int) {
+      self tmp = *this;
+      now_node = now_node->next;
+      return tmp;
+    }
+
+    value_type &operator*() { return now_node->value; }
+    value_type *operator->() { return &now_node->value; }
+
+    bool operator==(const iterator &other) const {
+      return this->now_node == other.now_node;
+    }
+    bool operator!=(const iterator &other) const {
+      return this->now_node != other.now_node;
+    }
+
+    self operator+(int n) {
+      self tmp = *this;
+      for (int i = 0; i < n; i++)
+        tmp++;
+      return tmp;
+    }
+
+    self operator-(int n) {
+      self tmp = *this;
+      for (int i = 0; i < n; i++)
+        tmp--;
+      return tmp;
+    }
+
+    self &operator=(const self &other) {
+      now_node = other.now_node;
+      return *this;
+    }
+    self &operator=(const self &&other) {
+      now_node = other.now_node;
+      return *this;
+    }
+
+  private:
+    list_node *now_node;
+  };
 
 public:
   // 别名定义
@@ -119,8 +217,8 @@ public:
   void push_back(const value_type &);
   void push_front(const value_type &);
 
-  template <typename... Args> void emplace_back(Args... args);
-  template <typename... Args> void emplace_front(Args... args);
+  template <typename... Args> void emplace_back(Args&&... args);
+  template <typename... Args> void emplace_front(Args&&... args);
 
   void pop_back();
   void pop_front();
@@ -128,36 +226,48 @@ public:
   iterator erase(iterator first, iterator last);
 
   void clear();
-  void resize();
+  void resize(size_t n);
 
-  iterator begin(){return iterator(static_cast<list_node*>(head.next));}
-  const iterator cbegin();
-  iterator end();
-  const iterator cend();
+  iterator begin() { return iterator(head.next); }
+  const iterator cbegin() const { return iterator(head.next); }
+  iterator end() { return iterator(head); }
+  const iterator cend() const { return iterator(head); }
 
-  reverse_iterator rbegin();
-  const reverse_iterator crbegin();
-  reverse_iterator rend();
-  const reverse_iterator crend();
+  reverse_iterator rbegin() { return reverse_iterator(head.prev); }
+  const reverse_iterator crbegin() const { return reverse_iterator(head.prev); }
+  reverse_iterator rend() { return reverse_iterator(head); }
+  const reverse_iterator crend() const { return reverse_iterator(head); }
 
-  value_type &front();
-  const value_type front() const;
-  value_type &back();
-  const value_type back() const;
+  value_type &front() {
+    assert(!empty());
+    return head.next->value;
+  }
+  const value_type& front() const {
+    assert(!empty());
+    return head.next->value;
+  }
+  value_type &back() {
+    assert(!empty());
+    return head.prev->value;
+  }
+  const value_type& back() const {
+    assert(!empty());
+    return head.prev->value;
+  }
 
-  bool empty() const;
+  bool empty() const { return head.next == &head; }
 
   // 析构函数
-  ~list() =default;
+  ~list();
 
 protected:
   // 申请n个空间并且使用value初始化他们
   void allocate_and_fill_value(list_head head, size_t n,
                                const value_type &value);
   list_node *construct(const value_type &value);
-  template <typename... Args>
-  list_node *construct_in_palce(Args... args);
-  void deconstruct(list_node*);
+  template <typename... Args> list_node *construct_in_palce(Args... args);
+  void deconstruct(list_node *);
+
 private:
   list_head head;
   Default_allocator allocator;
@@ -173,6 +283,7 @@ void list<T, Default_allocator>::push_back(const value_type &tmp) {
 
   new_node->next = &head;
   head.prev = new_node;
+  this->_size++;
 }
 
 template <typename T, typename Default_allocator>
@@ -185,11 +296,12 @@ void list<T, Default_allocator>::push_front(const value_type &tmp) {
 
   new_node->prev = &head;
   head.next = new_node;
+  this->_size++;
 }
 
 template <typename T, typename Default_allocator>
 template <typename... Args>
-void list<T, Default_allocator>::emplace_back(Args... args) {
+void list<T, Default_allocator>::emplace_back(Args&&... args) {
 
   list_node *new_node = construct_in_palce(std::forward<Args>(args)...);
   (*static_cast<list_node *>(head.prev)).next = new_node;
@@ -197,17 +309,19 @@ void list<T, Default_allocator>::emplace_back(Args... args) {
 
   new_node->next = &head;
   head.prev = new_node;
+  this->_size++;
 }
 
 template <typename T, typename Default_allocator>
 template <typename... Args>
-void list<T, Default_allocator>::emplace_front(Args... args) {
+void list<T, Default_allocator>::emplace_front(Args&&... args) {
   list_node *new_node = construct_in_palce(std::forward<Args>(args)...);
   (*static_cast<list_node *>(head.next)).prev = new_node;
   new_node->next = head.next;
 
   new_node->prev = &head;
   head.next = new_node;
+  this->_size++;
 }
 template <typename T, typename Default_allocator>
 void list<T, Default_allocator>::pop_back() {
@@ -215,13 +329,60 @@ void list<T, Default_allocator>::pop_back() {
   list_node *tmp = head.prev;
   tmp->prev->next = &head;
   head.prev = tmp->prev;
+  this->_size--;
   deconstruct(tmp);
 }
 
-    // 申请空间并且使用value填满
-    template <typename T, typename Default_allocator>
-    void list<T, Default_allocator>::allocate_and_fill_value(
-        list_head head, size_t n, const value_type &value) {
+template <typename T, typename Default_allocator>
+void list<T, Default_allocator>::pop_front() {
+  // 先析构对应的数值
+  list_node *tmp = head.next;
+  tmp->next->prev = &head;
+  head.next = tmp->next;
+  this->_size--;
+  deconstruct(tmp);
+}
+template <typename T, typename Default_allocator>
+typename list<T, Default_allocator>::iterator
+list<T, Default_allocator>::erase(iterator pos) {
+  list_node *tmp = pos.new_node;
+  tmp->prev->next = tmp->next;
+  tmp->next->prev = tmp->prev;
+  iterator ret(tmp->next);
+  deconstruct(tmp);
+  this->_size--;
+  return ret;
+}
+template <typename T, typename Default_allocator>
+typename list<T, Default_allocator>::iterator
+list<T, Default_allocator>::erase(iterator first, iterator last) {
+  while (first != last) {
+    first = erase(first);
+    this->_size--;
+  }
+  return last;
+}
+template <typename T, typename Default_allocator>
+void list<T, Default_allocator>::clear() {
+  iterator first = this->begin();
+  iterator last = this->end();
+  while (first != last) {
+    first = erase(first);
+  }
+  this->_size = 0;
+}
+template <typename T, typename Default_allocator>
+void list<T, Default_allocator>::resize(size_t n) {
+  if (n <= _size) {
+    // 裁剪
+    erase(begin(), begin() + n);
+  }
+}
+
+// 申请空间并且使用value填满
+template <typename T, typename Default_allocator>
+void list<T, Default_allocator>::allocate_and_fill_value(
+    list_head head, size_t n, const value_type &value) {
   if constexpr (std::is_trivially_constructible_v<value_type>()) {
     for (size_t i = 0; i < n; i++) {
       // 先构造node之后链接
@@ -265,18 +426,22 @@ template <typename... Args>
 typename list<T, Default_allocator>::list_node *
 list<T, Default_allocator>::construct_in_palce(Args... args) {
   list_node *new_node =
-      static_cast<list_node *>(allocator.allocate(sizeof(value_type)));
+      static_cast<list_node *>(allocator.allocate(sizeof(list_node)));
   new (&new_node->value) value_type(std::forward<Args>(args)...);
   return new_node;
 }
 template <typename T, typename Default_allocator>
-void
-list<T, Default_allocator>::deconstruct(list_node *node) {
-  if constexpr (std::is_trivially_destructible<value_type>()) {
+void list<T, Default_allocator>::deconstruct(list_node *node) {
+  if constexpr (!std::is_trivially_destructible<value_type>()) {
     node->value.~value_type();
   }
   allocator.deallocate(static_cast<void *>(node), sizeof(value_type));
   return;
+}
+template <typename T, typename Default_allocator>
+list<T, Default_allocator>::~list() {
+  clear();
+  deconstruct(&head);
 }
 
 }; // namespace m_stl
--- my_list/src/test.cpp
+++ my_list/src/test.cpp
@@ -1,8 +1,230 @@
-#include "../include/my_list.h"
+#include "my_list.h"
+#include <cassert>
 #include <iostream>
+
+// 打印当前测试用例进度
+#define TEST_HEADER()                                                          \
+  (std::cout << "=== Running Test Case: " << __func__ << " ===" << std::endl)
+
+// 打印失败信息
+#define TEST_FAILED()                                                          \
+  (std::cout << "❌ Assertion Failed in " << __func__ << " at line "           \
+             << __LINE__ << std::endl)
+
+// 打印成功信息
+#define TEST_PASSED()                                                          \
+  (std::cout << "✅ Test Passed: " << __func__ << std::endl << std::endl)
+
+// 测试基础构造函数和 push_back/push_front
+void test_constructor_and_push() {
+  TEST_HEADER();
+  {
+    m_stl::list<int> lst;
+    assert(lst.size() == 0);
+    assert(lst.empty());
+
+    lst.push_back(1);
+    assert(lst.size() == 1);
+    assert(lst.front() == 1 && lst.back() == 1);
+
+    lst.push_back(2);
+    assert(lst.size() == 2);
+    assert(lst.front() == 1 && lst.back() == 2);
+
+    lst.push_front(0);
+    assert(lst.size() == 3);
+    assert(lst.front() == 0 && lst.back() == 2);
+  }
+
+  {
+    m_stl::list<int> lst(3, 5);
+    assert(lst.size() == 3);
+    assert(lst.front() == 5 && lst.back() == 5);
+
+    auto it = lst.begin();
+    assert(*it++ == 5 && *it++ == 5 && *it++ == 5);
+  }
+
+  TEST_PASSED();
+}
+
+// 测试拷贝构造和拷贝赋值
+void test_copy_operations() {
+  TEST_HEADER();
+  {
+    m_stl::list<int> lst1(3, 10);
+    m_stl::list<int> lst2 = lst1; // 拷贝构造
+
+    assert(lst2.size() == 3);
+    for (auto it = lst2.begin(); it != lst2.end(); ++it) {
+      assert(*it == 10);
+    }
+
+    lst1.push_back(20); // 修改原始列表
+    assert(lst1.back() == 20);
+    assert(lst2.back() == 10); // 确保拷贝独立
+  }
+
+  {
+    m_stl::list<std::string> lst1;
+    lst1.push_back("hello");
+    lst1.push_back("world");
+
+    m_stl::list<std::string> lst2;
+    lst2 = lst1; // 拷贝赋值
+
+    assert(lst2.size() == 2);
+    assert(*lst2.begin() == "hello" && *(++lst2.begin()) == "world");
+
+    lst1.push_back("!");
+    assert(lst2.size() == 2); // 确保赋值后独立
+  }
+
+  TEST_PASSED();
+}
+
+// 测试移动构造和移动赋值
+void test_move_operations() {
+  TEST_HEADER();
+  {
+    m_stl::list<int> lst1;
+    lst1.push_back(100);
+    lst1.push_back(200);
+
+    m_stl::list<int> lst2 = std::move(lst1); // 移动构造
+
+    assert(lst2.size() == 2);
+    assert(lst2.front() == 100 && lst2.back() == 200);
+    assert(lst1.empty()); // 原列表应为空
+  }
+
+  {
+    m_stl::list<std::string> lst1;
+    lst1.push_back("move");
+
+    m_stl::list<std::string> lst2;
+    lst2 = std::move(lst1); // 移动赋值
+
+    assert(lst2.size() == 1);
+    assert(lst2.front() == "move");
+    assert(lst1.empty());
+  }
+
+  TEST_PASSED();
+}
+
+// 测试 emplace 操作
+void test_emplace() {
+  TEST_HEADER();
+  {
+    m_stl::list<std::pair<int, std::string>> lst;
+    lst.emplace_back(42, "life");
+    lst.emplace_front(7, "secrets");
+
+    assert(lst.size() == 2);
+    assert(lst.front().first == 7);
+    assert(lst.back().first == 42 && lst.back().second == "life");
+  }
+
+  TEST_PASSED();
+}
+
+// 测试 pop_back/pop_front/clear
+void test_erase_clear() {
+  TEST_HEADER();
+  {
+    m_stl::list<int> lst;
+    lst.push_back(1);
+    lst.push_back(2);
+    lst.push_back(3);
+
+    lst.pop_back();
+    assert(lst.size() == 2);
+    assert(lst.back() == 2);
+
+    lst.pop_front();
+    assert(lst.size() == 1);
+    assert(lst.front() == 2);
+
+    lst.push_back(3);
+    lst.push_back(4);
+    lst.clear();
+    assert(lst.empty());
+  }
+
+  TEST_PASSED();
+}
+
+// 测试 erase 和 resize
+void test_resize_erase() {
+  TEST_HEADER();
+  {
+    m_stl::list<int> lst;
+    lst.push_back(1);
+    lst.push_back(2);
+    lst.push_back(3);
+    lst.push_back(4);
+
+    // 测试 erase 单个元素
+    auto it = lst.erase(lst.begin());
+    assert(*it == 2 && lst.size() == 3 && lst.front() == 2);
+
+    // 测试 erase 范围
+    lst.erase(lst.begin(), lst.end());
+    assert(lst.empty());
+  }
+
+  {
+    m_stl::list<int> lst(5, 1); // 5个1
+    lst.resize(3);
+    assert(lst.size() == 3);
+    for (auto x : lst)
+      assert(x == 1);
+
+    lst.resize(5, 0);
+    assert(lst.size() == 5);
+    assert(lst.front() == 1 && lst.back() == 0);
+  }
+
+  TEST_PASSED();
+}
+
+// 测试迭代器遍历（包括 reverse）
+void test_iterators() {
+  TEST_HEADER();
+  {
+    m_stl::list<int> lst;
+    lst.push_back(10);
+    lst.push_back(20);
+    lst.push_back(30);
+
+    // 正向迭代器
+    int arr[] = {10, 20, 30};
+    int idx = 0;
+    for (auto it = lst.begin(); it != lst.end(); ++it) {
+      assert(*it == arr[idx++]);
+    }
+
+    // 反向迭代器
+    idx = 2;
+    for (auto it = lst.rbegin(); it != lst.rend(); ++it) {
+      assert(*it == arr[idx--]);
+    }
+  }
+
+  TEST_PASSED();
+}
+
+// 主测试程序
 int main() {
-  m_stl::list<int> list;
-  list.emplace_back(1);
-  std::cout << *list.begin();
-  list.pop_back();
+  test_constructor_and_push();
+  test_copy_operations();
+  test_move_operations();
+  test_emplace();
+  test_erase_clear();
+  test_resize_erase();
+  test_iterators();
+
+  std::cout << "All Tests Passed! 🎉" << std::endl;
+  return 0;
 }
